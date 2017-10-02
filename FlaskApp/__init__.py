70098797602111f79caf7aed42a86a0d71c8916d
Park\seunggak <qkrtmdrkr1@naver.com>
2017-10-02 23:52:31 +0900

    DB 테스트(2)

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -53,7 +53,7 @@ def menu():
 def list():
     c, conn = connection()
     c.execute("SELECT * FROM data")
-    d = c.fetchall()
+    d = c.fetchone()[0]
     return d
     # templist = []
     # for element in d:
